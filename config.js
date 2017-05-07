021b5616eb9372086b102f525da288eb785e1b76
sme_admin <dhafir.fuller@gmail.com>
2017-05-07 19:25:00 -0400

    fixed

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://dafear:sidney3@ds133311.mlab.com:33311/blog-apps'
+                       'mongodb://dafear:sidney3@ds133311.mlab.com:33311/blog-apps'
 exports.PORT = process.env.PORT || 5000;
+
 
