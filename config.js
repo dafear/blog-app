8a521f05eea828b11de2574f963169e9d2a6de3e
sme_admin <dhafir.fuller@gmail.com>
2017-05-07 22:50:59 -0400

    fixed

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       'mongodb://dafear:sidney3@ds133311.mlab.com:33311/blog-apps'
+exports.DATABASE_URL = 'mongodb://dafear:sidney3@ds133311.mlab.com:33311/blog-apps'
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-blog-app');
